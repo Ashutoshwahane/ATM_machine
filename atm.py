d141c9a73d9cfb9c0efdbc234dfac9d51cc77327
willskhalifa <50887729+Ashutoshwahane@users.noreply.github.com>
2019-07-11 16:04:38 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,11 +1,11 @@
 import math
 from pyfiglet import *
 print()
-figlet = figlet_format("WELCOME TO TMV ATM MACHINE",font="bubble")
+figlet = figlet_format("WELCOME TO TMV ATM MACHINE",font="bubble") #using figlet for message
 print(figlet)
 print()
 final_amount = None
-avail_balance = 100000
+avail_balance = 100000  #declaring the atm machine balance
 withdraw_money = 0
 twothousand_note = 50000
 two = None
@@ -17,32 +17,38 @@ fivehundred_note = 25000
 hundread_note = 20000
 fifty_note = 5000
 class atm:
-    def atm_machine(self):
+    def atm_machine(self):                              #creating function
         print("Available balance = ",avail_balance)
-        withdraw_money = int(input("how much you want to withdraw : "))
+        withdraw_money = int(input("how much you want to withdraw : "))     #getting the withdraw amount
+        temp = withdraw_money
         final_amount = avail_balance - withdraw_money
         figlet1 = figlet_format("your transaction is proccessing",font="bubble")
         print(figlet1)
-        two = math.floor(withdraw_money/2000)
+        two = math.floor(withdraw_money/2000)               #getting two thousand note
         print(2000, "*", two)
         for i in range(two):
-            withdraw_money = withdraw_money - 2000
-        fv = math.floor(withdraw_money/500)
+            withdraw_money = withdraw_money - 2000          #subtracting two thoundsand amount from withdraw money
+
+        fv = math.floor(withdraw_money/500)                 #getting five hundread note
         print(500, "*", fv)
         for i in range(fv):
-            withdraw_money = withdraw_money - 500
-        one = math.floor(withdraw_money/100)
+            withdraw_money = withdraw_money - 500           #subtracting five hundread amount from withdraw money
+
+        one = math.floor(withdraw_money/100)                #getting one hundread note
         print(100, "*", one)
         for i in range(one):
-            withdraw_money = withdraw_money - 100
-        fv = math.floor(withdraw_money/50)
+            withdraw_money = withdraw_money - 100           #subtracting one hundread amount from withdraw money
+
+
+        fv = math.floor(withdraw_money/50)                  #getting fifty note
         print(50, "*", fv)
         for i in range(fv):
-            withdraw_money = withdraw_money - 50
+            withdraw_money = withdraw_money - 50            #subtracting fity amount from withdraw money
 
-        print(withdraw_money)
+        print(temp)
         print("remaining balance in ATM machine : ",final_amount)
 
 a = atm()
 a.atm_machine()
+
 
